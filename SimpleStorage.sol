b8706a4c9874f64196197fe88e0c5dd37d3325ff
hanaibrahimmsheik <hanaibrahimmsheik@gmail.com>
2026-09-20 12:05:35 +0530

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.20; //compiler version to use
 
+//A simple contract that stores one number
 contract SimpleStorage {
+
+//Stores the number. "public" lets evryone read it from outside.
     int256 public value;
 
+//Increase the stored number by 1
     function increment() public {
         value += 1;
     }
 
+//Decreases the stored number by 1
     function decrement() public {
         value -= 1;
     }
